8841962821da76c82fedf456caf5e5b54e52352d
Blue Laptop <azxblue@o2.pl>
2023-01-25 14:43:21 +0100

    Fixed json problem

--- src/fakeDatabase.js
+++ src/fakeDatabase.js
@@ -1,30 +1,32 @@
-export let profileData = {
+const PUBLIC_URL = 'http://localhost:3000';
+
+export let users = [{
     id: '0',
     name: 'Adam',
-    profilePicUrl: `${process.env.PUBLIC_URL}/my-profile-pic.jpg`,
+    profilePicUrl: `${PUBLIC_URL}/my-profile-pic.jpg`,
     age: 27,
     bio: 'I like stuff',
     intrests: ['Programming', 'Cooking', 'Playin Gothic']
-}
+}]
 
 export let friends = [{
     id: '1',
     name: 'Aubrey Long',
-    profilePicUrl: `${process.env.PUBLIC_URL}/fren1.jpg`,
+    profilePicUrl: `${PUBLIC_URL}/fren1.jpg`,
     age: 40,
     bio: 'likes to eat',
     intrests: ['Cats', 'Dogs', 'Food']
-},{
+}, {
     id: '2',
     name: 'Eva Pena',
-    profilePicUrl: `${process.env.PUBLIC_URL}/fren2.jpg`,
+    profilePicUrl: `${PUBLIC_URL}/fren2.jpg`,
     age: 24,
     bio: 'likes to eat',
     intrests: ['Cats', 'Dogs', 'Frogs']
-},{
+}, {
     id: '3',
     name: 'Phyllis Carpenter',
-    profilePicUrl: `${process.env.PUBLIC_URL}/fren3.jpg`,
+    profilePicUrl: `${PUBLIC_URL}/fren3.jpg`,
     age: 31,
     bio: 'likes to paint',
     intrests: ['Landscapes', 'Human', 'Monke']
--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
-import express from 'express'
-import { profileData, friends, favorites } from './fakeDatabase'
+import express from 'express';
+import { users, friends, favorites } from './fakeDatabase';
 
 const app = express();
 app.use(express.json());
 
+app.get('/friends', (req, res) => {
+    res.json(friends);
+});
+
+app.get('/favorites', (req, res) => {
+    res.json(favorites);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+    const { friendId } = req.params;
+    const friend = friends.find(friend => friend.id == friendId);
+    if (friend) {
+        res.json(friend);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    const user = users.find(user => user.id == userId);
+    if (user) {
+        res.json(user);
+    } else {
+        res.sendStatus(404);
+    }
+});
 
 app.listen(8080, () => {
     console.log('Server is listening on post 8080');
-})
+});
