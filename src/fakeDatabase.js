ab6df2309f78516fbe85bf375ff95f6d12207a99
Blue PC <azxblue@o2.pl>
2023-02-06 06:45:08 +0100

    Adding MongoDB

--- src/fakeDatabase.js
+++ src/fakeDatabase.js
@@ -3,7 +3,7 @@ const PUBLIC_URL = 'http://localhost:3000';
 export let users = [{
     id: '0',
     name: 'Adam',
-    profilePicUrl: `${PUBLIC_URL}/my-profile-pic.jpg`,
+    profilePicUrl: `http://localhost:3000/my-profile-pic.jpg`,
     age: 27,
     bio: 'I like stuff',
     intrests: ['Programming', 'Cooking', 'Playin Gothic']
@@ -12,21 +12,21 @@ export let users = [{
 export let friends = [{
     id: '1',
     name: 'Aubrey Long',
-    profilePicUrl: `${PUBLIC_URL}/fren1.jpg`,
+    profilePicUrl: `http://localhost:3000/fren1.jpg`,
     age: 40,
     bio: 'likes to eat',
     intrests: ['Cats', 'Dogs', 'Food']
 }, {
     id: '2',
     name: 'Eva Pena',
-    profilePicUrl: `${PUBLIC_URL}/fren2.jpg`,
+    profilePicUrl: `http://localhost:3000/fren2.jpg`,
     age: 24,
     bio: 'likes to eat',
     intrests: ['Cats', 'Dogs', 'Frogs']
 }, {
     id: '3',
     name: 'Phyllis Carpenter',
-    profilePicUrl: `${PUBLIC_URL}/fren3.jpg`,
+    profilePicUrl: `http://localhost:3000/fren3.jpg`,
     age: 31,
     bio: 'likes to paint',
     intrests: ['Landscapes', 'Human', 'Monke']
--- src/server.js
+++ src/server.js
@@ -1,36 +1,47 @@
 import express from 'express';
 import { users, friends, favorites } from './fakeDatabase';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(express.json());
 
-app.get('/friends', (req, res) => {
-    res.json(friends);
-});
+const start = async () => {
 
-app.get('/favorites', (req, res) => {
-    res.json(favorites);
-});
+    const client = await MongoClient.connect('mongodb://localhost:27017/friend_tracker_db', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
-app.get('/friends/:friendId', (req, res) => {
-    const { friendId } = req.params;
-    const friend = friends.find(friend => friend.id == friendId);
-    if (friend) {
-        res.json(friend);
-    } else {
-        res.sendStatus(404);
-    }
-});
+    const db= client.db('friend_tracker_db');
 
-app.get('/users/:userId', (req, res) => {
-    const { userId } = req.params;
-    const user = users.find(user => user.id == userId);
-    if (user) {
-        res.json(user);
-    } else {
-        res.sendStatus(404);
-    }
-});
+    app.get('/friends', (req, res) => {
+        res.json(friends);
+    });
+
+    app.get('/favorites', (req, res) => {
+        res.json(favorites);
+    });
+
+    app.get('/friends/:friendId', (req, res) => {
+        const { friendId } = req.params;
+        const friend = friends.find(friend => friend.id == friendId);
+        if (friend) {
+            res.json(friend);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+    app.get('/users/:userId', (req, res) => {
+        const { userId } = req.params;
+        const user = users.find(user => user.id == userId);
+        if (user) {
+            res.json(user);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+}
 
 app.listen(8080, () => {
     console.log('Server is listening on post 8080');
