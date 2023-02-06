e702e35f3803a3e3540a48bc8bb43fdbde8085c7
Blue Laptop <azxblue@o2.pl>
2023-02-06 15:10:38 +0100

    Added working read endpoints for mongodb

--- src/server.js
+++ src/server.js
@@ -5,44 +5,45 @@ import { MongoClient } from 'mongodb';
 const app = express();
 app.use(express.json());
 
+var client = new MongoClient('mongodb://127.0.0.1:27017/friend_tracker_db');
+
 const start = async () => {
 
-    const client = await MongoClient.connect('mongodb://localhost:27017/friend_tracker_db', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    const db = client.db('friend_tracker_db');
 
-    const db= client.db('friend_tracker_db');
+    app.get('/friends', async (req, res) => {
+        const friends = await db.collection('friends').find({}).project({
+            name: 1,
+            age: 1,
+            profilePicUrl: 1
+        }).toArray();
 
-    app.get('/friends', (req, res) => {
         res.json(friends);
     });
 
-    app.get('/favorites', (req, res) => {
+    app.get('/favorites', async (req, res) => {
+        const favorites = await db.collection('favorites').find({}).toArray();
+
         res.json(favorites);
     });
 
-    app.get('/friends/:friendId', (req, res) => {
+    app.get('/friends/:friendId', async (req, res) => {
         const { friendId } = req.params;
-        const friend = friends.find(friend => friend.id == friendId);
-        if (friend) {
-            res.json(friend);
-        } else {
-            res.sendStatus(404);
-        }
+        const friend = await db.collection('friends').findOne({id: friendId});
+
+        res.json(friend);
     });
 
-    app.get('/users/:userId', (req, res) => {
+    app.get('/users/:userId', async (req, res) => {
         const { userId } = req.params;
-        const user = users.find(user => user.id == userId);
-        if (user) {
-            res.json(user);
-        } else {
-            res.sendStatus(404);
-        }
+        const user = await db.collection('users').findOne({id: userId});
+        res.json(user);
+    });
+
+
+    app.listen(8080, () => {
+        console.log('Server is listening on post 8080');
     });
 }
 
-app.listen(8080, () => {
-    console.log('Server is listening on post 8080');
-});
+start();
